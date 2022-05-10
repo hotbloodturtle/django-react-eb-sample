2176b277be3ff5a42c326c609cea78a398f0109a
Bale-haeseok <hs.kang@fitpet.co.kr>
2022-05-10 20:37:43 +0900

    fix - settings urls

---
+++ README.md
@@ -0,0 +1,8 @@
+### stack
+
+- python 3.8
+
+
+
+### env
+- SERVER_MODE=dev
--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.views.generic import TemplateView
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'),name='index'),
+    path('alive', lambda request: HttpResponse("I'm alive...")),
+]
+
+urlpatterns += [
+    re_path(r'^(?P<path>.*)$', TemplateView.as_view(template_name='index.html')),
 ]
