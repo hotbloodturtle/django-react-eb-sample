2d061ce5752dea49fcb0342ea242432b720d22ae
Bale-haeseok <hs.kang@fitpet.co.kr>
2022-07-15 13:44:41 +0900

    fix - root urls, static media

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 from django.urls import path, re_path
 from django.views.generic import TemplateView
 from django.http import HttpResponse
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('alive', lambda request: HttpResponse("I'm alive...")),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += [
     re_path(r'^(?P<path>.*)$', TemplateView.as_view(template_name='index.html')),
